7f31ecee0c4a08724322fe600caed0f2dbfccb1f
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-06-18 00:54:58 -0300

    feat: add validations to User attributes

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,8 +1,20 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class User {
   id: number;
+
+  @IsNotEmpty({ message: 'Username is required' })
+  @IsString({ message: 'Username must be a string' })
   username: string;
+
+  @IsEmail({}, { message: 'You must provide a valid email' })
   email: string;
+
+  @IsNotEmpty({ message: 'Password is required' })
   password: string;
+
+  @IsNotEmpty({ message: 'Fullname is required' })
   fullname: string;
+
   signupDate: Date;
 }
