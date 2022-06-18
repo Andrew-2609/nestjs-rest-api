8f7ba2d518d5c7ea49c2671bf2b2730b671450ce
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-06-18 00:45:45 -0300

    feat: create a function to return an User by username

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,7 +3,20 @@ import { User } from './user.entity';
 
 @Injectable()
 export class UserService {
-  private users: User[] = [];
+  private users: User[] = [
+    {
+      id: 1,
+      username: 'ndrewcoding',
+      email: 'andrew@email.com',
+      password: '123456_hack_me',
+      fullname: 'Andrew',
+      signupDate: new Date(),
+    },
+  ];
+
+  public getByUsername(username: string): User {
+    return this.users.find((user) => user.username === username);
+  }
 
   public create(user: User): User {
     this.users.push(user);
