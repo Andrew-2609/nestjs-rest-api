1a0bdbec43c3de6a4280fe0af09aa220e3023c43
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-06-18 13:41:49 -0300

    feat: add IsUnique constraint to username and email

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,13 +1,16 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsUnique } from './is-unique-validator';
 
 export class User {
   id: number;
 
   @IsNotEmpty({ message: 'Username is required' })
   @IsString({ message: 'Username must be a string' })
+  @IsUnique({ message: 'Username is already taken' })
   username: string;
 
   @IsEmail({}, { message: 'You must provide a valid email' })
+  @IsUnique({ message: 'Email is already in use' })
   email: string;
 
   @IsNotEmpty({ message: 'Password is required' })
