b4ff79ef21c07c2061f38081b7a73c499454147e
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-06-18 13:42:14 -0300

    feat: add IsUniqueConstraint to UserModule providers

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { IsUniqueConstraint } from './is-unique-validator';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, IsUniqueConstraint],
 })
 export class UserModule {}
