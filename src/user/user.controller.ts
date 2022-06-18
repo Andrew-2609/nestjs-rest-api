bc929a1571e84b87b396224f3f982eec681c9a6b
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-06-18 00:46:01 -0300

    feat: define an endpoint to return an User by its username

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { User } from './user.entity';
 import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
+
+  @Get(':username')
+  getByUsername(@Param('username') username: string): User {
+    return this.userService.getByUsername(username);
+  }
 
   @Post()
   create(@Body() user: User): User {
