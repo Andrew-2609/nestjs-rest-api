45f5da12cbb84532d42c68c57f46ae7d1c7ebdc5
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-06-18 00:34:21 -0300

    feat: define basic function to create an user

---
+++ src/user/user.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+
+@Injectable()
+export class UserService {
+  private users: User[] = [];
+
+  public createUser(user: User): User {
+    this.users.push(user);
+    return user;
+  }
+}
