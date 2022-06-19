2a0ed97c2a96e292f74a281e8358b872baedccb5
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-06-19 20:07:08 -0300

    feat: utilize ClassSerializerInterceptor as a provider in UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { IsUniqueConstraint } from './is-unique-validator';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, IsUniqueConstraint],
+  providers: [
+    UserService,
+    IsUniqueConstraint,
+    { provide: APP_INTERCEPTOR, useClass: ClassSerializerInterceptor },
+  ],
 })
 export class UserModule {}
