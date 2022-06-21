aab3d3bf785b91c689802f73da9ef30b607adfce
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-06-20 23:07:53 -0300

    feat: use HttpExceptionFilter as an AppModule provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './common/filters/http-exception-filter.filter';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UserModule],
   controllers: [],
-  providers: [],
+  providers: [{ provide: APP_FILTER, useClass: HttpExceptionFilter }],
 })
 export class AppModule {}
