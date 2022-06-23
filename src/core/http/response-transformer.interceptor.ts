7398363d7c297c7122607d707beee57ba4d3b150
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-06-22 23:45:23 -0300

    feat: implement and export interceptor for NestResponse with custom status, headers and body

---
+++ src/core/http/response-transformer.interceptor.ts
@@ -0,0 +1,49 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { AbstractHttpAdapter, HttpAdapterHost } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { NestResponse } from './nest-response';
+
+@Injectable()
+export class ResponseTransformerInterceptor implements NestInterceptor {
+  private readonly httpAdapter: AbstractHttpAdapter;
+
+  constructor(httpAdaterHost: HttpAdapterHost) {
+    this.httpAdapter = httpAdaterHost.httpAdapter;
+  }
+
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(
+      map((controllerResponse: NestResponse) => {
+        if (controllerResponse instanceof NestResponse) {
+          const httpContext = context.switchToHttp();
+          const response = httpContext.getResponse();
+
+          const { status, headers, body } = controllerResponse;
+
+          // Location, Owner, etc.
+          const headerNames = Object.getOwnPropertyNames(headers);
+
+          headerNames.forEach((headerName) => {
+            const headerValue = headers[headerName];
+            this.httpAdapter.setHeader(response, headerName, headerValue);
+          });
+
+          this.httpAdapter.status(response, status);
+
+          return body;
+        }
+
+        return controllerResponse;
+      }),
+    );
+  }
+}
