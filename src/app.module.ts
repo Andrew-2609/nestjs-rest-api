48061577d0efb3b985d5af6160dc7285436766bb
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-06-22 23:47:05 -0300

    feat: move and call ClassSerializerInterceptor and ResponseTransformerInterceptor in the App Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
-import { APP_FILTER } from '@nestjs/core';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { HttpExceptionFilter } from './common/filters/http-exception-filter.filter';
+import { ResponseTransformerInterceptor } from './core/http/response-transformer.interceptor';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UserModule],
   controllers: [],
-  providers: [{ provide: APP_FILTER, useClass: HttpExceptionFilter }],
+  providers: [
+    { provide: APP_FILTER, useClass: HttpExceptionFilter },
+    { provide: APP_INTERCEPTOR, useClass: ClassSerializerInterceptor },
+    { provide: APP_INTERCEPTOR, useClass: ResponseTransformerInterceptor },
+  ],
 })
 export class AppModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,15 +1,10 @@
-import { ClassSerializerInterceptor, Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { Module } from '@nestjs/common';
 import { IsUniqueConstraint } from './is-unique-validator';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [
-    UserService,
-    IsUniqueConstraint,
-    { provide: APP_INTERCEPTOR, useClass: ClassSerializerInterceptor },
-  ],
+  providers: [UserService, IsUniqueConstraint],
 })
 export class UserModule {}
